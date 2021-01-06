cee4177db86bd9fe8e9be0d5f4908f37ead8b598
Shobit1502 <72808545+Shobit1502@users.noreply.github.com>
2021-01-06 19:42:50 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="signup.php">Sign Up</a>
+        <a class="nav-link active" aria-current="page" href="index.php">Sign Up</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="login.php">Sign-In</a>
